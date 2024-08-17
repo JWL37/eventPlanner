e23a3b0006f7da91d8c97444651cf8cd2ed0fefb
Ruslan <nach.rus990@gmail.com>
2024-08-17 23:14:25 +0300

    update comment

--- internal/repositories/eventRepository.go
+++ internal/repositories/eventRepository.go
@@ -3,9 +3,7 @@ package repositories
 import "eventPlanner/internal/models"
 
 type EventRepository interface {
-	// CreateEvent создаёт мероприятие для его организатора
+	// CreateEvent создаёт мероприятие для всех участников мероприятия(организатор в том числе)
 	CreateEvent(userID int64, event models.Event) error
 	GetAllEvents(userID int64) ([]models.Event, error)
-	// CreateEventMembers создаёт одно и тоже мероприятие для каждого участника
-	//CreateEventMembers(userID int64, implemEventRepository models.Event) error
 }
